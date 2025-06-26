961bc00e028fceee2c147702cb2e61b7ad7723ab
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-06-26 11:35:53 +0200

    MAJ JS avec condition de victoire/défaite, animation et finish de la fonction tentative

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,28 +36,103 @@ const motsPendu = [
     "beatles", "nirvana", "bts", "marvel", "dccomics", "pixar", "disney", "netflix", "pixl", "ghibli",
     "nintendo", "playstation", "xbox", "hbo", "tiktok", "youtube", "twitch", "funko", "lego", "barbie"
 ];
-let random = Math.floor(Math.random() * 200)
+let random = Math.floor(Math.random() * motsPendu.length)
 let motDevine = []
 let motCacher = []
 let erreur = 0
+let initialisation = 0
 
-motDevine = motsPendu[random].split('')
-for (let i = 0; i < motDevine.length; i++) {
-    motCacher[i] = ' _ '
+
+
+
+
+// Cache le mot pour le joueur
+while (initialisation != 1) {
+
+    motDevine = motsPendu[random].toUpperCase().split('')
+
+    for (let i = 0; i < motDevine.length; i++) {
+        motCacher[i] = ' _ '
+    }
+    console.log(motDevine)
+    document.getElementById("motADevine").textContent = motCacher.join("")
+    initialisation = 1
 }
-console.log(motDevine)
-document.getElementById("motADevine").textContent = motCacher
 
+
+
+//Le jeu
 function tentative(lettre) {
-    motDevine.forEach(function (lettre, index) {
-        if (lettre == motDevine[index]) {
-            motCacher[index] = motDevine[index]
-        } else {
-            erreur = erreur + 1
-            document.getElementById("pendu").src = `assets/img/pendu${erreur}.png`
+    let trouve = false;
+
+    //Cherche si la lettre est existante dans le mot pour renvoie true
+    motDevine.forEach(function (char, index) {
+        if (lettre == char) {
+            motCacher[index] = char
+            trouve = true
         }
     })
+    document.getElementById("motADevine").textContent = motCacher.join("")
+
+
+    //Reward ou punition par rapport au true
+    if (trouve) {
+        document.getElementById(lettre).style.backgroundColor = "gold"
+    } else {
+        erreur = erreur + 1;
+        document.getElementById("pendu").src = `assets/img/pendu${erreur}.png`
+        document.getElementById(lettre).style.backgroundColor = "gray"
+    }
+
+    //Condition de victoire ou de défaite
+
+    const areEqual = motCacher.every((value, index) => value === motDevine[index]) //Verifie si les 2 tableaux sont identique (booléen)
+
+    if (areEqual == 1) {
+        console.log("win")
+        victoire()
+    } else if (erreur == 9) {
+        gameOver()
+    }
+
 }
+
+
+function victoire() {
+    document.getElementById("motADevine").textContent = motDevine.join("")
+    document.getElementById("abandon").style.display = "none"
+    document.getElementById("rejouer").style.display = "inline"
+    document.getElementById("confetti").style.display = "inline"
+    for (let i = 0; i < 12500; i++) {
+        let p = i % 25
+        setTimeout(() => {
+            document.getElementById("pendu").src = (i % 2 === 0) ? "assets/img/win1.png" : "assets/img/win2.png";
+        }, i * 200)
+        setTimeout(() => {
+            document.getElementById("confetti").src = `assets/img/Confetti/5811153573a8496be24aec27f9ae16c5CsrgYiH554PN9B5c-${p}.png`
+        }, i * 50)
+        if (document.getElementById("rejouer").onclick == true) {
+            rejouer()
+        }
+    }
+}
+
+function gameOver() {
+    document.getElementById("pendu").src = `assets/img/pendu9.png`
+    document.getElementById("gameOver").style.display = "inline"
+    document.getElementById("clavier1").style.display = "none"
+    document.getElementById("clavier2").style.display = "none"
+    document.getElementById("clavier3").style.display = "none"
+    document.getElementById("abandon").style.display = "none"
+    document.getElementById("rejouer").style.display = "inline"
+    document.getElementById("motADevine").textContent = motDevine.join("")
+}
+
+function rejouer() {
+    window.location.reload()
+}
+
+
 
 function Nuit() {
     document.getElementById("body").style.color = "white"
